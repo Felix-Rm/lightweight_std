1e99bceb4a8458ef0c1c265e49b843e2fc3a4f9b
Felix Reissmann <felix.rm.153@gmail.com>
2022-02-24 12:57:38 +0100

    Added implementaion of 'std::unique_ptr'

---
+++ unique_ptr.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+namespace lw_std {
+
+template <typename T>
+class unique_ptr {
+   private:
+    T* m_ptr{nullptr};
+
+   public:
+    unique_ptr() = default;
+
+    unique_ptr(const T& ptr)
+        : m_ptr(ptr){};
+
+    unique_ptr(const unique_ptr&&) = delete;
+
+    ~unique_ptr() {
+        if (m_ptr) delete m_ptr;
+    }
+
+    unique_ptr(unique_ptr&& other) {
+        operator=(other);
+    }
+
+    operator bool() const {
+        return m_ptr != nullptr;
+    }
+
+    unique_ptr& operator=(unique_ptr&& other) {
+        if (this != &other) {
+            if (m_ptr) delete m_ptr;
+            m_ptr = other.m_ptr;
+            other.m_ptr = nullptr;
+        }
+        return *this;
+    }
+
+    unique_ptr& operator=(T* ptr) {
+        if (m_ptr) delete m_ptr;
+        m_ptr = ptr;
+
+        return *this;
+    }
+
+    T& operator->() {
+        return *m_ptr;
+    }
+
+    T& operator*() {
+        return *m_ptr;
+    }
+
+    T* get() {
+        return m_ptr;
+    }
+
+    void reset(T new_obj) {
+        operator=(&new_obj);
+    }
+
+    T* release() {
+        auto tmp = m_ptr;
+        m_ptr = nullptr;
+        return tmp;
+    }
+};
+}  // namespace lw_std
