880ed1e22c156b8afd39637618e1d94944329d16
Felix Reissmann <felix.rm.153@gmail.com>
2022-05-01 14:24:42 +0200

    Move number of operations per container into a variable

--- tests/testing.cpp
+++ tests/testing.cpp
@@ -7,23 +7,25 @@
 #include "test_lw_vector.hpp"
 
 int main() {
+    static constexpr size_t num_operations = 10000;
+
     TestLogging::start_suite("lw_std");
 
-    TestLogging::run("vector<int>", TestLwVector::run_with_int, 1000);
-    TestLogging::run("vector<NonTrivial>", TestLwVector::run_with_non_trivial, 1000);
+    TestLogging::run("vector<int>", TestLwVector::run_with_int, num_operations);
+    TestLogging::run("vector<NonTrivial>", TestLwVector::run_with_non_trivial, num_operations);
 
-    TestLogging::run("list<int>", TestLwList::run_with_int, 1000);
-    TestLogging::run("list<NonTrivial", TestLwList::run_with_non_trivial, 1000);
+    TestLogging::run("list<int>", TestLwList::run_with_int, num_operations);
+    TestLogging::run("list<NonTrivial", TestLwList::run_with_non_trivial, num_operations);
 
     TestLogging::run("pair", TestLwPair::run);
 
-    TestLogging::run("queue<int>", TestLwQueue::run_with_int, 1000);
-    TestLogging::run("queue<NonTrivial>", TestLwQueue::run_with_non_trivial, 1000);
+    TestLogging::run("queue<int>", TestLwQueue::run_with_int, num_operations);
+    TestLogging::run("queue<NonTrivial>", TestLwQueue::run_with_non_trivial, num_operations);
 
-    TestLogging::run("unordered_set<int>", TestLwUnorderedSet::run_with_int, 1000);
+    TestLogging::run("unordered_set<int>", TestLwUnorderedSet::run_with_int, num_operations);
 
-    TestLogging::run("unordered_map<int, int>", TestLwUnorderedMap::run_with_int_int, 1000);
-    TestLogging::run("unordered_map<int, NonTrivial>", TestLwUnorderedMap::run_with_int_non_trivial, 1000);
+    TestLogging::run("unordered_map<int, int>", TestLwUnorderedMap::run_with_int_int, num_operations);
+    TestLogging::run("unordered_map<int, NonTrivial>", TestLwUnorderedMap::run_with_int_non_trivial, num_operations);
 
     return TestLogging::results();
 }
