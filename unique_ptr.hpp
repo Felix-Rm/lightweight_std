50680a5ee8913a2b1f7fa6413d95096641d0b6eb
Felix Reissmann <felix.rm.153@gmail.com>
2022-05-01 13:21:03 +0200

    Add const versions of `operator->` and `operator*`

--- unique_ptr.hpp
+++ unique_ptr.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "utility.hpp"
+
 namespace lw_std {
 
 template <typename T>
@@ -10,20 +12,17 @@ class unique_ptr {
    public:
     unique_ptr() = default;
 
-    unique_ptr(const T& ptr)
+    unique_ptr(T* ptr)
         : m_ptr(ptr){};
 
-    unique_ptr(const unique_ptr&&) = delete;
-
-    ~unique_ptr() {
-        if (m_ptr) {
-            delete m_ptr;
-            m_ptr = nullptr;
-        };
-    }
+    unique_ptr(const unique_ptr&) = delete;
 
     unique_ptr(unique_ptr&& other) {
-        operator=(other);
+        operator=(lw_std::move(other));
+    }
+
+    ~unique_ptr() {
+        destroy();
     }
 
     operator bool() const {
@@ -50,7 +49,15 @@ class unique_ptr {
         return *m_ptr;
     }
 
+    const T& operator->() const {
+        return *m_ptr;
+    }
+
     T& operator*() {
+        return *m_ptr;
+    }
+
+    const T& operator*() const {
         return *m_ptr;
     }
 
@@ -58,14 +65,25 @@ class unique_ptr {
         return m_ptr;
     }
 
-    void reset(T new_obj) {
-        operator=(&new_obj);
+    const T* get() const {
+        return m_ptr;
+    }
+
+    void reset(T* new_obj) {
+        operator=(new_obj);
     }
 
     T* release() {
         auto tmp = m_ptr;
         m_ptr = nullptr;
         return tmp;
+    }
+
+    void destroy() {
+        if (m_ptr) {
+            delete m_ptr;
+            m_ptr = nullptr;
+        };
     }
 };
 }  // namespace lw_std
