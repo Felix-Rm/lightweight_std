ad3b2fb48b9a769c45e08279ae7832f2f01fc9f1
Felix Reissmann <felix.rm.153@gmail.com>
2022-05-01 14:00:57 +0200

    Add limits for unsigned 32bit and 64bit ints

--- limits.hpp
+++ limits.hpp
@@ -38,4 +38,30 @@ class numeric_limits<uint16_t> {
     };
 };
 
+// lw_std implementation of std::limits for uint32_t
+template <>
+class numeric_limits<uint32_t> {
+   public:
+    static constexpr uint32_t max() {
+        return 4294967295L;
+    };
+
+    static constexpr uint32_t min() {
+        return 0;
+    };
+};
+
+// lw_std implementation of std::limits for uint64_t
+template <>
+class numeric_limits<uint64_t> {
+   public:
+    static constexpr uint64_t max() {
+        return 18446744073709551615UL;
+    };
+
+    static constexpr uint64_t min() {
+        return 0;
+    };
+};
+
 }  // namespace lw_std
