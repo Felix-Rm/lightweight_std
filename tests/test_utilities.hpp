855a3382984e50572d33919b6f4f05fa4c5f6450
Felix Reissmann <felix.rm.153@gmail.com>
2022-05-01 14:15:45 +0200

    Add some memory management to `NonTrivial`
    This should make incorrect usage of methods and destructors more obvious while testing.

--- tests/test_utilities.hpp
+++ tests/test_utilities.hpp
@@ -11,27 +11,73 @@
 
 #include "ftest/test_logging.hpp"
 
+/*
+    This is a non default-constructable object with heap allocations to check if destructor is called aproproately
+*/
+
 class NonTrivial {
    public:
-    NonTrivial() = delete;
+    NonTrivial() {
+        m_data = new unsigned{(unsigned) -1};
+    };
 
-    NonTrivial(unsigned a)
-        : m_data(a){};
+    NonTrivial(unsigned a) {
+        m_data = new unsigned{a};
+    }
+
+    // NonTrivial(const NonTrivial&) = delete;
+    NonTrivial(const NonTrivial& other) {
+        operator=(other);
+    }
+
+    NonTrivial(NonTrivial&& other) {
+        operator=(std::move(other));
+    }
+
+    ~NonTrivial() {
+        clear();
+    }
+
+    NonTrivial& operator=(const NonTrivial& other) {
+        if (this != &other) {
+            clear();
+            if (other.m_data)
+                m_data = new unsigned{*other.m_data};
+        }
+        return *this;
+    };
+
+    NonTrivial& operator=(NonTrivial&& other) {
+        if (this != &other) {
+            clear();
+            m_data = other.m_data;
+            other.m_data = nullptr;
+        }
+        return *this;
+    };
 
     bool operator==(const NonTrivial& rhs) const {
-        return m_data == rhs.m_data;
+        if (m_data == nullptr || rhs.m_data == nullptr) return false;
+        return *m_data == *rhs.m_data;
     };
 
     bool operator!=(const NonTrivial& rhs) const {
         return !operator==(rhs);
     };
 
-    auto data() const {
-        return m_data;
+    unsigned data() const {
+        return *m_data;
     };
 
+    void clear() {
+        if (m_data) {
+            delete m_data;
+            m_data = nullptr;
+        }
+    }
+
    private:
-    unsigned m_data{0};
+    unsigned* m_data{nullptr};
 };
 
 template <typename TestContainer, typename VerifyContainer, typename ContainerValueType = typename VerifyContainer::value_type>
